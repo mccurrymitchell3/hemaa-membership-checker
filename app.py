f5784238f8018be9fc2f9a9ffa35a7dc27c0ffd8
Rahul Gautam <rgautam6@gatech.edu>
2020-04-07 13:40:57 -0400

    Flask routing complete

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect, request, flash
+from flask import Flask, render_template, url_for, redirect, request
 from forms import EmailInput
 from main import valid_email
 
@@ -6,28 +6,23 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '29cb8631d5b4cf1a13f4355798845a93'
 
-@app.route('/hello')
-def hello():
-    return render_template('%s.html' % 'flashmessage')
-
 @app.route('/', methods=['GET', 'POST'])
 def home_page():
     form = EmailInput(request.form)
     if form.validate_on_submit():
         email_address = request.form['email']
-        if (valid_email(email_address)):
-            return redirect(url_for('valid', email_address=email_address))
-        return redirect(url_for('invalid', email_address=email_address))
-
+        if(valid_email(email_address)):
+            return redirect(url_for('valid', email=email_address))
+        return redirect(url_for('invalid', email=email_address))
     return render_template('%s.html' % "emailform", form=form)
 
-@app.route('/valid=<email_address>')
-def valid(email_address):
-    return '%s is a valid member' % email_address
+@app.route('/valid<email>')
+def valid(email):
+    return render_template('%s.html' % 'valid', email=email)
 
-@app.route('/invalid=<email_address>')
-def invalid(email_address):
-    return '%s is not a valid member' % email_address
+@app.route('/invalid<email>')
+def invalid(email):
+    return render_template('%s.html' % 'invalid', email=email)
 
 
 if __name__ == "__main__":
